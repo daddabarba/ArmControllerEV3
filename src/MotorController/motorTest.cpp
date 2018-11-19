ff70238794b2f985a32321bac130ae7c03e6413c
daddabarba <davidebarbieri97@gmail.com>
2018-11-19 01:32:18 +0100

    motor test

--- src/MotorController/motorTest.cpp
+++ src/MotorController/motorTest.cpp
@@ -1,10 +1,13 @@
 #include "Class/MotorController.hpp"
 
+#include <unistd.h>
+
 int main()
 {
 	ev3TachoController::MotorController motor(0);
 
-	motor.goTo(180, 200);
+	motor.setHold();
+	motor.makeStep(180, 200);
 
 	return 0;
 }
